b356af4fc0171b47b66a7afc2146ad1a69f58e52
Luca Fiore <lf44551@gmail.com>
2026-02-20 13:18:48 +0100

    Sposta la logica delle route per la gestione dei post nel controller

--- app.js
+++ app.js
@@ -1,104 +1,12 @@
 const express = require('express');
-const connection = require('./data/db');
+const postsController = require('./controllers/postsController');
 
 const app = express();
 const port = 3000;
 
-function runQueryWithTimeout(sql, params, callback, timeoutMs = 5000) {
-  let isHandled = false;
-
-  const timer = setTimeout(() => {
-    if (isHandled) {
-      return;
-    }
-
-    isHandled = true;
-    callback(new Error('Timeout query database'));
-  }, timeoutMs);
-
-  connection.query(sql, params, (err, results) => {
-    if (isHandled) {
-      return;
-    }
-
-    isHandled = true;
-    clearTimeout(timer);
-    callback(err, results);
-  });
-}
-
-app.get('/', (req, res) => {
-  const sql = 'SELECT * FROM posts';
-
-  runQueryWithTimeout(sql, [], (err, results) => {
-    if (err) {
-      return res.status(500).json({
-        error: 'Errore nel recupero dei post dal database',
-        details: err.message
-      });
-    }
-
-    res.json(results);
-  });
-});
-
-app.get('/posts/:id', (req, res) => {
-  const postId = Number(req.params.id);
-
-  if (!Number.isInteger(postId) || postId <= 0) {
-    return res.status(400).json({
-      error: 'ID non valido'
-    });
-  }
-
-  const sql = 'SELECT * FROM posts WHERE id = ?';
-
-  runQueryWithTimeout(sql, [postId], (err, results) => {
-    if (err) {
-      return res.status(500).json({
-        error: 'Errore nel recupero del post dal database',
-        details: err.message
-      });
-    }
-
-    if (results.length === 0) {
-      return res.status(404).json({
-        error: 'Post non trovato'
-      });
-    }
-
-    res.json(results[0]);
-  });
-});
-
-app.delete('/posts/:id', (req, res) => {
-  const postId = Number(req.params.id);
-
-  if (!Number.isInteger(postId) || postId <= 0) {
-    return res.status(400).json({
-      error: 'ID non valido'
-    });
-  }
-
-  const sql = 'DELETE FROM posts WHERE id = ?';
-
-  runQueryWithTimeout(sql, [postId], (err, result) => {
-    if (err) {
-      return res.status(500).json({
-        error: "Errore nell'eliminazione del post",
-        details: err.message
-      });
-    }
-
-    if (result.affectedRows === 0) {
-      return res.status(404).json({
-        error: 'Post non trovato'
-      });
-    }
-
-    res.sendStatus(204);
-  });
-});
+app.get('/', postsController.index);
+app.get('/posts/:id', postsController.show);
+app.delete('/posts/:id', postsController.destroy);
 
 app.listen(port, () => {
   console.log(`Server in ascolto su http://localhost:${port}`);
